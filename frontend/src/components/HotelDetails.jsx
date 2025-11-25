b9c7ee90c4e6f662d624b7015349103fe48b28d0
Thanmayee <radhathanmayee@gmail.com>
2025-11-25 14:25:32 +0530

    addedloading

--- frontend/src/components/HotelDetails.jsx
+++ frontend/src/components/HotelDetails.jsx
@@ -6,7 +6,7 @@ import Cookies from 'js-cookie'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import { BeatLoader } from 'react-spinners';
+import { ClipLoader } from 'react-spinners';
 
 
 
@@ -44,7 +44,7 @@ function HotelDetails(props) {
     const [reviewList , setReviewList] = useState([])
     const [review , setReview] = useState('')
     const [guests , setGuests] = useState('')
-    const [loading , setLoading] = useState(true)
+    const [loading , setLoading] = useState(false)
     const [rating, setRating] = useState('')
     const [errorMsg , setErrorMsg] = useState('')
 
@@ -85,15 +85,23 @@ function HotelDetails(props) {
      
        
       function func(){
-          async function hoteldetails(){
-            setLoading(true)
-            const response = await fetch(`${port}/hotel/review/${id}`)
-            const jsonData = await response.json()
-            console.log(jsonData.reviews)
-            setReviewList(jsonData.reviews)
-            
+        async function hoteldetails(){
+            try{
+              setLoading(true)
+              const response = await fetch(`${port}/hotel/review/${id}`)
+              const jsonData = await response.json()
+              console.log(jsonData.reviews)
+              setReviewList(jsonData.reviews)
+            }
+        
+        catch(e){
+            console.log(e)
         }
-        hoteldetails()
+        finally{
+          setLoading(false)
+        }
+      }
+      hoteldetails()
     }
     useEffect(func , [id])
 
@@ -106,13 +114,6 @@ function HotelDetails(props) {
 
          const userId = Cookies.get("userId")
          
-      //   const newReview = {
-      //   id : reviewList.length + 1,
-      //   review,
-      // }
-      // setReviewList(prev => [...prev , newReview])
-      // setReview('')
-
         const response = await fetch(`${port}/hotel/review` , {
           method : "POST",
           headers : {
@@ -193,10 +194,6 @@ function HotelDetails(props) {
             setTotalPrice(tp)
             console.log("total price : ",tp) 
            
-          //  if (!checkIn || !checkOut || !guests || !details.price) {
-          //     alert("Please select dates and number of guests first.");
-          //     return 
-          //   }
             if(!data.success){
          setErrorMsg(data.message)
       }
@@ -211,8 +208,14 @@ function HotelDetails(props) {
 
     
   return (
+    loading ?  
+      (  
+     <div className="flex justify-center items-center min-h-screen bg-white">
+        <ClipLoader color="#2563eb" size={50} />
+      </div>
+      ):
     <>
-        
+       
     <div className='flex flex-row text-blue-700 ml-3 bg-gray-50 pt-5 pb-1'>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className='h-7 w-7 pt-1 pr-1 pl-2' fill="blue">
       <path d="M73.4 297.4C60.9 309.9 60.9 330.2 73.4 342.7L233.4 502.7C245.9 515.2 266.2 515.2 278.7 502.7C291.2 490.2 291.2 469.9 278.7 457.4L173.3 352L544 352C561.7 352 576 337.7 576 320C576 302.3 561.7 288 544 288L173.3 288L278.7 182.6C291.2 170.1 291.2 149.8 278.7 137.3C266.2 124.8 245.9 124.8 233.4 137.3L73.4 297.3z"/></svg>
@@ -429,7 +432,7 @@ function HotelDetails(props) {
          </div>
        </div>
     </div>  
-
+    
     
       
    </> 
